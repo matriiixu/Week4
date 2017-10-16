f41adf3ccfe34d92a85961011e42238d6b72a379
matriiixu <oani1_16@uni.worc.ac.uk>
2017-10-16 11:57:12 +0100

    Update 1.2

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,17 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+var key = "pen";
+var value = "blue";
 
-document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
-function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
+window.localStorage.setItem(key, value);
+
+
+
+
+
+var trigger = document.getElementById('trigger');
+function outputFunction(){
+    document.getElementById('key').innerHTML="<br/><hr><span>Your key is:&nbsp</span>" + key + "<br/><hr>";
+    document.getElementById('value').innerHTML="<span>Your value is:&nbsp</span>" + value + "<br/><hr>";
 }
-
-
-// device APIs are available
-//
-    function onDeviceReady() {
-	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	updateDisplay();
-	    
-	alert("device ready");
-    }
-
-
-    function onPause() {
-	
-	paused_count++;
-	updateDisplay();
-	    
-	alert("pause");
-    }
-	
-
-    function onResume() {
-		
-	resumed_count++;
-	updateDisplay();
-	    
-	alert("resume");
-    }
+function numberFunction(){
+    document.getElementById('numbers').innerHTML="<span>Your local storage contains:&nbsp</span>" + window.localStorage.length + "<span>&nbspitems.</span><br/><hr>";
+}
